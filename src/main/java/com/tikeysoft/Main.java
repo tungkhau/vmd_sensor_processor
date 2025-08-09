d295165555ecc0d00a9f853d9cc20e4a34a38fc5
Tung Khau <55617424+tungkhau@users.noreply.github.com>
2025-08-09 12:27:51 +0200

    update auto connect

--- src/main/java/com/tikeysoft/Main.java
+++ src/main/java/com/tikeysoft/Main.java
@@ -1,16 +1,11 @@
 package com.tikeysoft;
 
 import com.tikeysoft.aem.AemDataHandler;
-import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class Main {
     public static void main(String[] args) {
         String broker = "tcp://host.docker.internal:1883";
         AemDataHandler mqttHandler = new AemDataHandler();
-        try {
-            mqttHandler.connectAndSubscribe(broker);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
+        mqttHandler.connectAndSubscribe(broker);
     }
 }
